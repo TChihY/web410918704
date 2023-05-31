339a7e3d1d97d934f53b089de3a6e94cdbab7592
鄧芷允 <s1091870@gm.pu.edu.tw>
2023-05-31 19:57:34 +0800

    0531

---
+++ Web410918704/Models/Partials/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web410918704.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+    }
+    public class StudentMetadata
+    {
+        public int Id{ get; set; }
+
+        [Required(ErrorMessage ="請填寫姓名")]
+        [Display(Name ="姓名")]
+        [StringLength(10,ErrorMessage ="請勿超過10個字串")]
+        public string Name{ get; set; }
+
+        [Required(ErrorMessage = "請填寫學號")]
+        [Display(Name = "學號")]
+        [StringLength(10, ErrorMessage = "請勿超過10個字串")]
+        public string Number{ get; set; }
+
+        [Required(ErrorMessage = "請填寫 Email")]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email{ get; set; }
+
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "是否註冊")]
+        public bool IsActive{ get; set; }
+    }
+}
